e0bd78e3142d3ca41505d27d5b13488db09bac00
slithek <jayrandolph9@yahoo.com>
2017-01-23 16:42:04 -0500

    Uncommented original demo code

--- src/comp110/Demo.java
+++ src/comp110/Demo.java
@@ -5,27 +5,27 @@ public class Demo {
   public static void main(String[] args) {
 
     // Default Console (must be imported if not in comp110 package)
-//    Console.out.print("Hello, world");
-//
-//    if (!Console.out.promptBoolean("Proceed? (true or false)")) {
-//      Console.out.print("goodbye.");
-//      return;
-//    }
-//
-//    Console console = new Console("Another window");
-//    console.speed(0.1);
-//    console.print(console);
-//    console.print("Drag me over so you can see both consoles...");
-//
-//    String name = Console.out.promptString("First name") + " " + Console.out.promptString("Last name");
-//    console.print("You entered: " + name);
-//
-//    while (console.promptBoolean("Continue? (true/false)")) {
-//      double x = console.promptDouble();
-//      console.print(x * x);
-//    }
-//
-//    console.print("goodbye.");
+    Console.out.print("Hello, world");
+
+    if (!Console.out.promptBoolean("Proceed? (true or false)")) {
+      Console.out.print("goodbye.");
+      return;
+    }
+
+    Console console = new Console("Another window");
+    console.speed(0.1);
+    console.print(console);
+    console.print("Drag me over so you can see both consoles...");
+
+    String name = Console.out.promptString("First name") + " " + Console.out.promptString("Last name");
+    console.print("You entered: " + name);
+
+    while (console.promptBoolean("Continue? (true/false)")) {
+      double x = console.promptDouble();
+      console.print(x * x);
+    }
+
+    console.print("goodbye.");
 	  
 
 	  Console c = new Console("COMP110");
